d0457bc1b1fa5cdaf8f63e61b98a1a8d28ed1218
Natasha Osborne <natasha08@me.com>
2016-05-15 17:00:11 -0600

    repo for project with sensitive files

---
+++ public/javascripts/require.js
@@ -0,0 +1,10 @@
+var mysql = require('mysql');
+
+var pool = mysql.createPool({
+	connectionLimit : 100,
+	host: 'localhost',
+	user: 'tulsi',
+	password: 'Yoni3454!',
+	database: 'freetools'
+
+});
