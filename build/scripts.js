cdc8556dd8d918008c3dcff525e5d07ce8dc90c2
Natasha Osborne <natasha08@me.com>
2016-05-16 07:09:01 -0600

    ...

--- build/scripts.js
+++ build/scripts.js
@@ -322,3 +322,13 @@ module.exports = shownutritiontable;
 module.exports = createFood;
 module.exports = showFood;
 },{}]},{},[1]);
+
+var mysql      = require('mysql');
+var pool = mysql.createConnection({
+  host     : 'localhost',
+  user     : 'tulsi',
+  password : 'Yoni3454!',
+  database : 'freetools'
+});
+
+module.exports = pool;
