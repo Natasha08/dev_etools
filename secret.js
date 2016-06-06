d2fe97a66a6ada2549ea6801ce251ba34c18a29c
Natasha Osborne <natasha08@me.com>
2016-06-06 09:44:56 -0600

    deleted superfluous practice code

--- secret.js
+++
@@ -1,6 +0,0 @@
-var natskey  = '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';
-
-module.exports = natskey;
-
-// var sendgrid_api = require('../secret');
-// var sendgrid  = require('sendgrid')(sendgrid_api);
