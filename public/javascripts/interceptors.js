ac3cc3353d2783bc52368cdffb4be8a4c377c60f
Natasha Osborne <natasha08@me.com>
2016-06-06 09:29:24 -0600

    added a token interceptor, controllers with json data and for logging in and out. Services is a placeholder: will add service for backend

---
+++ public/javascripts/interceptors.js
@@ -0,0 +1,21 @@
+
+
+myApp.factory('Auth', (['$q', '$location', '$localStorage', function($q, $location, $localStorage) {
+		return {
+			 'request': function (config) {
+				config.headers = config.headers || {};
+				if ($localStorage.token) {
+					config.headers.Authorization = 'Bearer' + $localStorage.token;
+				}
+				return config;
+			},
+			 'responseError': function(response) {
+			 	if(response.status === 401 || response.status === 403) {
+			 		$location.path('/login');
+			 	}
+			 	return $q.reject(response);
+			}
+		};
+		
+}
+]));
